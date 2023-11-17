247ee20812e3539aaaa9d1a9e1ff01bd766b172d
Rahul <86316159+rahul17081998@users.noreply.github.com>
2023-11-17 13:11:22 +0530

    Added Navigation structure

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,9 +2,9 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="bg-cyan-600 rounded  max-w-prose pb-8">
+     
         MovieList      
-      </div>
+     
     </main>
   )
 }
