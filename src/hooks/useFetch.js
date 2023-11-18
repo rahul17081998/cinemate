fa0b9026625e679e04b9eecbf41d76418a5c1c56
Rahul <86316159+rahul17081998@users.noreply.github.com>
2023-11-18 14:31:07 +0530

    Added movie search api fetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 // rafc
-export const useFetch = (apiPath) => {
+export const useFetch = (apiPath, queryTerm="") => {
 
     const[data, setData] = useState([]);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
     useEffect(()=>{
         async function fetchMovies(){
           const response = await fetch(url);
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,25 @@
-export const Search = () => {
+import { useSearchParams } from "react-router-dom";
+import { Card } from "../components"
+import { useFetch } from "../hooks/useFetch";
+
+
+export const Search = ({ apiPath }) => {
+  const[searchParams]=useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = useFetch(apiPath, queryTerm);
+
   return (
     <main>
-      Search
+      <section>
+        <p className="text-3xl text-gray-700 dark:text-white">{movies.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'`}</p>
+      </section>
+      <section className="max-w-7px mx-auto py-7">
+        <div className="flex justify-start flex-wrep max-w-screen-xl mx-auto grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
