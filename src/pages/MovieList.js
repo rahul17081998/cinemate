1280195b3c8340709afd0684ea92ae6c2a346ef5
Rahul <86316159+rahul17081998@users.noreply.github.com>
2023-11-19 01:05:38 +0530

    Added Tailwind customization

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -10,7 +10,7 @@ export const MovieList = ({apiPath}) => {
   return (
     <main>
       <section className="max-w-7px mx-auto py-7">
-        <div className="flex justify-start flex-wrep max-w-screen-xl mx-auto grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+        <div className="flex justify-start flex-wrep max-w-screen-xl mx-auto grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 other:justify-evenly">
           {movies.map((movie) =>(
             <Card key={movie.id} movie={movie} />
           ))}
