9eb260b478f1a64ac60ae19e450e25269aecaad9
Rahul <86316159+rahul17081998@users.noreply.github.com>
2023-11-19 11:00:36 +0530

    Added scroll to top functionality

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
+import { ScrollToTop } from "./ScrollToTop";
+
 export { Header } from "./Header";
 export { Footer } from "./Footer";
+
 export { Card } from "./Card";
 export {Button} from "./Button";
+
+export {ScrollToTop} from "./ScrollToTop";
